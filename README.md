e6d999ec7bf3fb3a0ca2ed5170c393cabdee1858
Isha Kasliwal <isha.kasliwal@gmail.com>
2015-12-04 12:40:03 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,71 @@
+Content Generator for Sketch
+============================
+
+Sketch generator allows you quickly create dummy data such as avatars, names, geo location data etc.
+
+## Demo
+#####Generating pictures
+
+![Generating thumbnails](https://raw.githubusercontent.com/timuric/Content-generator-for-sketch-app/master/tutorial/userpics.gif)
+
+#####Generating names
+
+![Generating names](https://raw.githubusercontent.com/timuric/Content-generator-for-sketch-app/master/tutorial/names.gif)
+
+#####Generating dummy text
+
+![Generating text](https://raw.githubusercontent.com/timuric/Content-generator-for-sketch-app/master/tutorial/lorem.gif)
+
+#####Generating strings
+![custom string](https://cloud.githubusercontent.com/assets/5709624/8092928/c18d6d76-0fbd-11e5-962d-417165cc1a2d.gif)
+
+You can create a custom string by combining any of these options:
+- plain text (example: banana)
+- random number from range (example: [0-20] )
+- random item from an array (example: [banana~apple~grapes] )
+
+An example of a combined string: $[0-999],[10-99] [new~used~old]
+
+Which would result in "$321,34 old" or "$12,75 new"
+
+
+## Generating custom images
+Use `Photos\Custom` plugin to specify path to your own folder with images
+
+
+## Installation
+1. Download Zip and Extract it to a folder
+2. In Sketch app `Plugins > Manage Plugins...`
+3. In the Preferences window, click on the cog-wheel icon, and select `Show Plugins Folder` from the dropdown.
+4. Place the extracted folder directly to the Plugins folder (nesting might not work atm)
+
+## Upcoming features
+* More types of data
+* Fetch content directly from different online sources
+* Easier extensibility
+
+## Feature requests & feedback
+Ping me on [twitter](http://twitter.com/timur_carpeev) or follow for updates.
+
+## Bug reports
+1. Open Console app `Applications/Utilities/Console`
+2. Type Sketch In filter box [like so](https://raw.githubusercontent.com/timuric/Content-generator-sketch-plugin/master/tutorial/console.png)
+3. Run broken plugin
+4. Submit output of the Console app via [Github issues](https://github.com/timuric/Content-generator-sketch-plugin/issues) or [twitter](http://twitter.com/timur_carpeev) toghether with the version of your Sketch app
+
+
+## Contributors
+Author [Timur Carpeev](https://twitter.com/timur_carpeev)
+
+Number plugins [Liu Liu](https://twitter.com/auxdesigner)
+
+And [others](https://github.com/timuric/Content-generator-sketch-plugin/graphs/contributors)
+
+## Credits
+* Photo collection [Unsplash](http://unsplash.com/)
+* User pics [Uifaces](http://uifaces.com/)
+* User names [Uinames](http://uinames.com/)
+* Random data [Mockaroo](http://mockaroo.com/)
+* Custom photos [Ian Silber](https://github.com/iansilber/sketch-image-replace)
+* Vintage user pics [Rad faces](http://www.radfaces.com/)
+* Flickr keywords [https://github.com/nickstamas](https://github.com/nickstamas/Sketch-Quick-Pic)
---
+++ js/utility.js
@@ -0,0 +1,104 @@
+var tools = {
+	appVersion: "0.1",
+	versionComponents : function() {
+		var info = [[NSBundle mainBundle] infoDictionary];
+		var items = [[(info["CFBundleShortVersionString"]) componentsSeparatedByString:"."] mutableCopy];
+
+		while([items count] < 3)
+			[items addObject:"0"];
+
+		return items;
+	},
+	majorVersion : function() {
+		var items = tools.versionComponents();
+
+		return items[0];
+	},
+	minorVersion : function() {
+		var items = tools.versionComponents();
+
+		return items[1];
+	},
+	convertToString : function(objectString){
+		var i = 0;
+		normalString = "";
+		while(objectString[i] !== null) normalString += objectString[i];
+		return normalString;
+	},
+
+	saveFile : function(path,data){		
+		var someContent = NSString.stringWithString_(data)
+		var path = path
+		someContent.dataUsingEncoding_(NSUTF8StringEncoding).writeToFile_atomically_(path, true)
+	},
+	pluginPath : function(){
+		if(tools.majorVersion() == 3){
+			var pluginFolder = scriptPath.match(/Plugins\/([\w -])*/)[0] + "/";
+			var sketchPluginsPath = scriptPath.replace(/Plugins([\w \/ -])*.sketchplugin$/, "");
+			return pluginFolder;
+		}		
+	},
+	getJSONFromURL: function(url) {
+		var request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]],
+			response = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil],
+			responseObj = [NSJSONSerialization JSONObjectWithData:response options:nil error:nil]
+		return responseObj
+	}
+};
+
+function alert(msg, title) {
+  title = title || "Whoops";
+  var app = [NSApplication sharedApplication];
+  [app displayDialog:msg withTitle:title];
+}
+
+function deleteLayer(layer){
+	var parent = [layer parentGroup];
+	if(parent) [parent removeLayer: layer];
+}
+
+function capitalize(str) {
+  return str.slice(0, 1).toUpperCase() + str.slice(1);
+}
+
+function shuffle(array) {
+	var currentIndex = array.count(), temporaryValue, randomIndex ;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+function replaceWithImages(images, context) {
+	selection = context.selection;
+	for(var i = 0; i < [selection count]; i++) {
+
+		var image = [[NSImage alloc] initByReferencingFile:images[i]];
+		var layer = selection[i];
+		if([layer class] == MSShapeGroup){
+			var fill = layer.style().fills().firstObject();
+			fill.setFillType(4);
+			var coll = layer.style().fills().firstObject().documentData().images();
+			[fill setPatternImage:image collection:coll]
+			layer.style().fills().firstObject().setPatternFillType(1);
+		}
+	}
+
+	if([selection count] == 0) [doc showMessage:'Select at least one vector shape'];;
+
+}
+
+
+
+
